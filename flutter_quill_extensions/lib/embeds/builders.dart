2157c237527914c4850230c37eff3d8aef12e4b2
Franck Soudan <franck.soudan@jalis.fr>
2023-05-16 10:44:13 +0200

    getting rid of math_keyboard…

--- flutter_quill_extensions/lib/embeds/builders.dart
+++ flutter_quill_extensions/lib/embeds/builders.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_quill/extensions.dart' as base;
 import 'package:flutter_quill/flutter_quill.dart' hide Text;
 import 'package:flutter_quill/translations.dart';
 import 'package:gallery_saver/gallery_saver.dart';
-import 'package:math_keyboard/math_keyboard.dart';
 import 'package:universal_html/html.dart' as html;
 
 import '../shims/dart_ui_fake.dart'
@@ -211,39 +209,6 @@ class VideoEmbedBuilder extends EmbedBuilder {
       context: context,
       readOnly: readOnly,
       onVideoInit: onVideoInit,
-    );
-  }
-}
-
-class FormulaEmbedBuilder extends EmbedBuilder {
-  @override
-  String get key => BlockEmbed.formulaType;
-
-  @override
-  Widget build(
-    BuildContext context,
-    QuillController controller,
-    base.Embed node,
-    bool readOnly,
-    bool inline,
-  ) {
-    assert(!kIsWeb, 'Please provide formula EmbedBuilder for Web');
-
-    final mathController = MathFieldEditingController();
-    return Focus(
-      onFocusChange: (hasFocus) {
-        if (hasFocus) {
-          // If the MathField is tapped, hides the built in keyboard
-          SystemChannels.textInput.invokeMethod('TextInput.hide');
-          debugPrint(mathController.currentEditingValue());
-        }
-      },
-      child: MathField(
-        controller: mathController,
-        variables: const ['x', 'y', 'z'],
-        onChanged: (value) {},
-        onSubmitted: (value) {},
-      ),
     );
   }
 }
